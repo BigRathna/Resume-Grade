ea69997774059921d8e7afd621c5c10a9672f7ba
BigRathna <rathna.sabhapathy2020@vitstudent.ac.in>
2023-06-10 01:45:47 +0530

    modified:   app.py 	modified:   main.ipynb 	modified:   model traning.ipynb

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import PyPDF2
+import numpy
 import streamlit as st
 import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+import os
 import re
+import tempfile
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
+import nltk
 nltk.download('omw-1.4')
 
 # Function to preprocess the text
@@ -34,8 +38,12 @@ def preprocess_text(text):
     return processed_text
 
 def predict(pth):
-    
-    with open(pth, 'rb') as f:
+
+    tempo_file = tempfile.NamedTemporaryFile(delete=False)
+    tempo_file.write(pth.read())
+    tempo_file.close()
+    model = tf.keras.models.load_model('models/model_large.h5')
+    with open(tempo_file.name, 'rb') as f:
         pdf = PyPDF2.PdfReader(f)
         num_pages = len(pdf.pages)
         text = ''
@@ -52,23 +60,35 @@ def predict(pth):
     my_data_pad = pad_sequences(my_data_tokens, maxlen=max(len(seq) for seq in my_data_tokens), padding='post')
     
     predictions = model.predict(my_data_pad)
-
+    os.remove(tempo_file.name)
     return predictions[0]
 
 def main():
-    model = tf.keras.models.load_model('models/model_large.h5')
+    
     st.title("Resume Acceptance Prediction")
 
     # Upload PDF file
     uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
     
     if uploaded_file is not None:
-        # Preprocess and predict
-        predictions = predict(uploaded_file)
+        st.write("Analyzing resume...")
+        percentage = predict(uploaded_file)
+        percentage_rounded = numpy.round(percentage * 100, 2)
         
-        # Display predictions
-        st.write("Predicted Probability:", predictions)
-
+        # Display the percentage value with larger font size
+        st.markdown(f"<h1 style='text-align: center;'>{percentage_rounded}%</h1>", unsafe_allow_html=True)
+        
+        # Determine the emoji based on the percentage value
+        if percentage_rounded >= 80:
+            emoji = "😃"
+        elif percentage_rounded >= 60:
+            emoji = "🙂"
+        else:
+            emoji = "😕"
+        
+        # Display the emoji with larger font size
+        st.markdown(f"<h1 style='text-align: center;'>{emoji}</h1>", unsafe_allow_html=True)
+        
 # Run the app
 if __name__ == "__main__":
     main()
